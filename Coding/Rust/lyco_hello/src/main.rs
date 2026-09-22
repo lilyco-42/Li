d97bc31487e0b28e638c670694a4417c4e15aa27
lilyco-42 <251102081+lilyco-42@users.noreply.github.com>
2026-09-22 23:27:22 +0800

    移除了 lyco_hello

--- Coding/Rust/lyco_hello/src/main.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    lilyco42();
-}
-fn lilyco42() {
-    println!("Hello Lilyco42")
-}
--- Coding/Rust/lyco_hello/target/debug/lyco_hello.d
+++
@@ -1,1 +0,0 @@
-D:\Code\Li\Coding\Rust\lyco_hello\target\debug\lyco_hello.exe: D:\Code\Li\Coding\Rust\lyco_hello\src\main.rs
